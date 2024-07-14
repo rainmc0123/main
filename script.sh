ef060a6743783d28ff82113f25f5b24114a4c1a0
rainmc0123 <elsa.rgt22@gmail.com>
2024-07-14 13:35:54 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-TOKEN="your_personal_access_token"
-REPO="https://$TOKEN:x-oauth-basic@github.com/rainmc0123/rainmc0123.git"
+REPO="https://github.com/rainmc0123/rainmc0123.git"
 
 # Clone atau fetch dari repo
 git clone $REPO
